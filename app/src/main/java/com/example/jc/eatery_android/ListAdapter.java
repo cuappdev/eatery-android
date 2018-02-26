0d25f5164c86b32944cbf137c7f34ba391298a97
jc979 <jc979@cornell.edu>
2018-02-26 00:48:36 -0500

    CardView setup

--- app/src/main/java/com/example/jc/eatery_android/ListAdapter.java
+++ app/src/main/java/com/example/jc/eatery_android/ListAdapter.java
@@ -2,9 +2,12 @@ package com.example.jc.eatery_android;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by JC on 2/22/18.
@@ -14,15 +17,16 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListAdapterVie
 
     Context mContext;
     final private ListAdapterOnClickHandler mListAdapterOnClickHandler;
-
+    private int mCount;
     //needs to implment this method in MainActivity
     public interface ListAdapterOnClickHandler {
         void onClick(int position);
     }
 
-    public ListAdapter(Context context, ListAdapterOnClickHandler clickHandler) {
+    public ListAdapter(Context context, ListAdapterOnClickHandler clickHandler, int count) {
         mContext = context;
         mListAdapterOnClickHandler = clickHandler;
+        mCount = count;
 
     }
 
@@ -30,12 +34,10 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListAdapterVie
     public ListAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         //set this to layout of cardview
-        int layoutId= 0;
-
+        int layoutId= R.layout.custom_row;
 
         View view = LayoutInflater.from(mContext).inflate(layoutId,parent,false);
         view.setFocusable(true);
-
 
 
         return new ListAdapterViewHolder(view);
@@ -44,21 +46,35 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListAdapterVie
     @Override
     public void onBindViewHolder(ListAdapterViewHolder holder, int position) {
 
-        //set all the textView, ImageView in card view here
+        holder.mText.setText("does it work");
+        holder.mText2.setText("Yes it does");
+        holder.mImage.setImageResource(R.drawable.sample);
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        Log.i("Tag",""+mCount);
+        return mCount;
     }
 
     class ListAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         //define all textView + ImageViews in here
 
+        TextView mText;
+        TextView mText2;
+        ImageView mImage;
+
         public ListAdapterViewHolder(View itemView) {
             super(itemView);
+            mText = itemView.findViewById(R.id.cv_text1);
+            mText2 = itemView.findViewById(R.id.cv_text2);
+            mImage = itemView.findViewById(R.id.cv_image1);
+
+            itemView.setOnClickListener(this);
+
         }
 
         @Override
--- app/src/main/java/com/example/jc/eatery_android/MainActivity.java
+++ app/src/main/java/com/example/jc/eatery_android/MainActivity.java
@@ -5,15 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.jc.eatery_android.Model.CafeteriaModel;
 import com.example.jc.eatery_android.NetworkUtils.NetworkUtilities;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ListAdapter.ListAdapterOnClickHandler{
 
     public RecyclerView mRecyclerView;
 
@@ -21,17 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         new ProcessJson().execute("");
-
-        mRecyclerView = findViewById(R.id.cafe_list);
-
-        mRecyclerView.setHasFixedSize(true);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayout.VERTICAL,false);
-        mRecyclerView.setLayoutManager(layoutManager);
-
-
-
-
 
 
 
@@ -39,34 +30,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public class ProcessJson extends AsyncTask<String, Void, String>{
+    @Override
+    public void onClick(int position) {
+        Toast.makeText(this,""+position,Toast.LENGTH_SHORT).show();
+    }
+
+    public class ProcessJson extends AsyncTask<String, Void, ArrayList<CafeteriaModel>>{
+
 
         @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
+        protected ArrayList<CafeteriaModel> doInBackground(String... params) {
 
 
-            ArrayList<CafeteriaModel> test = NetworkUtilities.getJson();
-
-
-            for (CafeteriaModel object : test) {
-                if(object.getName().equals("Cook House Dining Room"))
-                    Log.i("tag", object.stringTo());
-            }
-
-
-            return null;
+            ArrayList<CafeteriaModel> cafeList = NetworkUtilities.getJson();
+            return cafeList;
 
 
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
+        protected void onPostExecute(ArrayList<CafeteriaModel> result) {
+            super.onPostExecute(result);
+
+            mRecyclerView = findViewById(R.id.cafe_list);
+
+            mRecyclerView.setHasFixedSize(true);
+            LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayout.VERTICAL,false);
+            mRecyclerView.setLayoutManager(layoutManager);
+
+            ListAdapter listAdapter = new ListAdapter(getApplicationContext(), MainActivity.this,result.size());
+            mRecyclerView.setAdapter(listAdapter);
 
 
         }
