01d2906942aa08ec839d04ce92755cc7c790ad7a
Abdullah Islam <ai.islam1433@gmail.com>
2018-04-22 13:11:25 -0400

    updated loading screen

--- app/src/main/java/com/example/jc/eatery_android/SplashActivity.java
+++ app/src/main/java/com/example/jc/eatery_android/SplashActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class  SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
