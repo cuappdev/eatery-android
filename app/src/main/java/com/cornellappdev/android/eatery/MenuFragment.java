8105c3c363fbed85b9cc8f1c765e97dcb815208e
Lesley Huang <ningning621@gmail.com>
2018-08-26 12:32:50 -0400

    Clean code

--- app/src/main/java/com/cornellappdev/android/eatery/MenuFragment.java
+++ app/src/main/java/com/cornellappdev/android/eatery/MenuFragment.java
@@ -26,51 +26,53 @@ public class MenuFragment extends Fragment {
     ArrayList<MealModel> menus;
     LinearLayout linear;
 
-    public MenuFragment() {
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         menus = (ArrayList<MealModel>) getArguments().getSerializable("cafeData");
-
         View view = inflater.inflate(R.layout.fragment_menu, container, false);
+        linear = view.findViewById(R.id.linearFragment);
+
         try {
             position = getArguments().getInt("position");
         } catch (Exception e) {
-          e.printStackTrace();
+            e.printStackTrace();
         }
 
-        linear = view.findViewById(R.id.linearFragment);
+        // Checks for unavailable or missing menus
+        if (menus.get(position).getMenu().entrySet().isEmpty()) {
+            TextView missingMenuText = new TextView(getContext());
+            missingMenuText.setText("No menu available");
+            missingMenuText.setTextSize(14);
+            linear.addView(missingMenuText);
+        }
 
         int counter = 0;
-
         for (HashMap.Entry<String, ArrayList<String>> entry : menus.get(position).getMenu().entrySet()) {
-
-            //add subheading for category of food
+            // Add subheading for category of food
             String key = entry.getKey();
             List<String> value = entry.getValue();
-            TextView tv = new TextView(getContext());
+            TextView categoryText = new TextView(getContext());
             SpannableString str = new SpannableString(key);
-            tv.setText(str);
-            tv.setAllCaps(true);
-            tv.setTextSize(18);
-            tv.setPadding(0, 60,0, 16);
-            linear.addView(tv);
+            categoryText.setText(str);
+            categoryText.setAllCaps(true);
+            categoryText.setTextSize(18);
+            categoryText.setPadding(0, 60,0, 16);
+            linear.addView(categoryText);
 
-            //adds individual meal items
+            // Add individual meal items to list
             for (int i = 0; i < value.size(); i++) {
-                TextView tv2 = new TextView(getContext());
-                tv2.setText(value.get(i));
-                tv2.setTextSize(14);
-                tv2.setPadding(0, 0, 0, 8);
+                TextView mealItemText = new TextView(getContext());
+                mealItemText.setText(value.get(i));
+                mealItemText.setTextSize(14);
+                mealItemText.setPadding(0, 0, 0, 8);
                 if (i == value.size() - 1) {
-                    tv2.setPadding(0, 0, 0, 60);
+                    mealItemText.setPadding(0, 0, 0, 60);
                 }
-                linear.addView(tv2);
+                linear.addView(mealItemText);
             }
 
-            //adds horizontal line
+            // Add horizontal line that separates each category
             if (counter != menus.get(position).getMenu().entrySet().size() - 1) {
                 View blank = new View(getContext());
                 blank.setBackgroundColor(Color.argb(100, 192,192, 192  ));
@@ -81,18 +83,8 @@ public class MenuFragment extends Fragment {
             }
             counter++;
         }
-        if (menus.get(position).getMenu().entrySet().isEmpty()) {
-            TextView tv = new TextView(getContext());
-            tv.setText("No menu available");
-            tv.setTextSize(14);
-            linear.addView(tv);
-        }
 
         // Inflate the layout for this fragment
-
         return view;
-
     }
-
-
 }
