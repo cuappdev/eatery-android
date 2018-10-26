94b0d054b44e2b53d970d5ca4f6004336f07910c
jc979 <jc979@cornell.edu>
2018-10-26 15:19:19 -0400

    Finished implementing basic info page, just need to apply design changes

--- app/src/main/java/com/cornellappdev/android/eatery/InfoActivity.java
+++ app/src/main/java/com/cornellappdev/android/eatery/InfoActivity.java
@@ -1,13 +1,29 @@
 package com.cornellappdev.android.eatery;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class InfoActivity extends AppCompatActivity {
 
+  private TextView mFeedbackText;
+  private TextView mWebsiteText;
+  private TextView mInfoText;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_info);
+    setTitle("About");
+
+    mInfoText = findViewById(R.id.infoText);
+    mFeedbackText = findViewById(R.id.feedbackText);
+    mWebsiteText = findViewById(R.id.websiteText);
+
+    mFeedbackText.setMovementMethod(LinkMovementMethod.getInstance());
+    mWebsiteText.setMovementMethod(LinkMovementMethod.getInstance());
+
   }
 }
