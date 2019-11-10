e8aa72b1b1ff7e28c7d6b56e8f93bf9dd73b51cb
Yanlam Ko <yk396@cornell.edu>
2019-11-10 13:46:56 -0500

    Instantiate onboarding info fragments within pager adapter constructor, so new fragments aren't being made each time getItem is called.

--- app/src/main/java/com/cornellappdev/android/eatery/onboarding/OnboardingPagerAdapter.java
+++ app/src/main/java/com/cornellappdev/android/eatery/onboarding/OnboardingPagerAdapter.java
@@ -8,22 +8,30 @@ import com.cornellappdev.android.eatery.model.enums.OnboardingPageType;
 
 public class OnboardingPagerAdapter extends FragmentPagerAdapter {
     private static int NUM_ITEMS = 4;
+    private OnboardingInfoFragment menusFragment;
+    private OnboardingInfoFragment collegetownFragment;
+    private OnboardingInfoFragment transactionsFragment;
+    private OnboardingInfoFragment loginFragment;
 
     public OnboardingPagerAdapter(FragmentManager fm) {
         super(fm);
+        menusFragment = new OnboardingInfoFragment(OnboardingPageType.MENUS);
+        collegetownFragment = new OnboardingInfoFragment(OnboardingPageType.COLLEGETOWN);
+        transactionsFragment = new OnboardingInfoFragment(OnboardingPageType.TRANSACTIONS);
+        loginFragment = new OnboardingInfoFragment(OnboardingPageType.LOGIN);
     }
 
     @Override
     public Fragment getItem(int position) {
         switch (position) {
             case 0:
-                return new OnboardingInfoFragment(OnboardingPageType.MENUS);
+                return menusFragment;
             case 1:
-                return new OnboardingInfoFragment(OnboardingPageType.COLLEGETOWN);
+                return collegetownFragment;
             case 2:
-                return new OnboardingInfoFragment(OnboardingPageType.TRANSACTIONS);
+                return transactionsFragment;
             case 3:
-                return new OnboardingInfoFragment(OnboardingPageType.LOGIN);
+                return loginFragment;
             default:
                 return null;
         }
