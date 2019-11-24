3bf8f6214013ab1aa58fad734bea88567f42da3f
yk396 <yk396@cornell.edu>
2019-11-24 13:46:45 -0500

    Add lottie code

--- app/src/main/java/com/cornellappdev/android/eatery/onboarding/OnboardingInfoFragment.java
+++ app/src/main/java/com/cornellappdev/android/eatery/onboarding/OnboardingInfoFragment.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 
 import com.cornellappdev.android.eatery.R;
 import com.cornellappdev.android.eatery.model.enums.OnboardingPageType;
-import com.cornellappdev.android.eatery.presenter.AccountPresenter;
 
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
