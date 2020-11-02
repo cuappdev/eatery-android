35493fcf3a1f2a90b4d6413dc04ac6fc0a9e06e7
Adam Kadhim <adamakadhim@gmail.com>
2020-11-02 16:54:12 -0500

    AndroidX migration and targetsdk and compilesdk are now version 29

--- app/src/main/java/com/cornellappdev/android/eatery/MainListFragment.java
+++ app/src/main/java/com/cornellappdev/android/eatery/MainListFragment.java
@@ -10,7 +10,6 @@ import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
