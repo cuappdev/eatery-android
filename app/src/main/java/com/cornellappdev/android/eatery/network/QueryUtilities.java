55f40a0c1ca9d589640fe62e5955d343c6a29734
Kevin Sun <contact@kevinsun.dev>
2021-08-30 23:34:20 -0400

    Update QueryUtilities.java

--- app/src/main/java/com/cornellappdev/android/eatery/network/QueryUtilities.java
+++ app/src/main/java/com/cornellappdev/android/eatery/network/QueryUtilities.java
@@ -16,16 +16,12 @@ import java.util.List;
 
 public final class QueryUtilities {
 
-    // These were manually set as the dining hall IDs based on the IDs returned with graphQL
-    private final static HashSet<Integer> DINING_HALL_IDS =
-            new HashSet<>(Arrays.asList(1, 7, 12, 21, 23, 27, 29, 30, 31, 32));
-
     static ArrayList<EateryBaseModel> parseEateries(List<AllEateriesQuery.Eatery> eateries,
                                                            Context mainContext) {
         ArrayList<EateryBaseModel> eateryList = new ArrayList<>();
         for (AllEateriesQuery.Eatery eatery : eateries) {
             EateryBaseModel model;
-            if (DINING_HALL_IDS.contains(eatery.id())) {
+            if (eatery.eateryType().equals("Dining Room")) {
                 model = DiningHallModel.fromEatery(mainContext, false, eatery);
             } else {
                 model = CafeModel.fromEatery(mainContext, false, eatery);
